3a13ec736d542fdfb5c2559414524d81d1dd2cfc
ewanzd <ewanzd@gmail.com>
2019-04-01 17:04:16 +0100

    add view

---
+++ AutomaticSummaryCreator/View/ConfigPresenter.cs
@@ -0,0 +1,15 @@
+﻿using AutomaticSummaryCreator.Services;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AutomaticSummaryCreator.View
+{
+    public class ConfigPresenter
+    {
+        private readonly IConfigView configView;
+        private readonly DataService dataService;
+    }
+}
---
+++ AutomaticSummaryCreator/View/IConfigView.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AutomaticSummaryCreator.View
+{
+    public interface IConfigView
+    {
+        ConfigPresenter Presenter { get; set; }
+    }
+}
